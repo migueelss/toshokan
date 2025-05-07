50ded998933e74d65b6825a02ae4490dd0e62d7b
Bruno Santos <bms.santos@outlook.pt>
2025-05-07 17:55:31 +0100

    Fetch!

--- src/routes/toshokan.svelte
+++ src/routes/toshokan.svelte
@@ -1,1 +1,48 @@
-<div>Toshokan</div>
+<script lang="ts">
+    type Anime = {
+      id: number;
+      title: {
+        romaji: string;
+        english: string;
+        native: string;
+      };
+    };
+  
+    let anime: Anime[] = [];
+  
+    fetch('https://graphql.anilist.co', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `
+          query ($search: String!) {
+            Page {
+              media(search: $search, type: ANIME) {
+                id
+                title {
+                  romaji
+                  english
+                  native
+                }
+              }
+            }
+          }
+        `,
+        variables: { search: "Naruto" }
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      anime = data.data.Page.media;
+    })
+    .catch(err => console.error(err));
+  </script>
+  
+  <h1>Toshokan</h1>
+  
+  {#if anime.length}
+    <div>{anime[0].title.native}</div>
+  {:else}
+    <div>Loading...</div>
+  {/if}
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    darkMode: 'class',
-    content: ['./**/*.html'],
+    darkMode: 'class', 
     theme: {
       extend: {},
     },
