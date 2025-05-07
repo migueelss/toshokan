020ab1f94e5c7ec366a13b8fe9fd53c6ebd853ee
Bruno Santos <bms.santos@outlook.pt>
2025-05-07 17:29:59 +0100

    toshokan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Toshokan from "./toshokan.svelte";
+</script>
+
+<nav class="bg-white p-4 flex justify-center gap-2">
+    <span class="text-blue-300 text-3xl">図書館</span><span class="text-black text-3xl font-semibold" style="font-family: 'Times New Roman', serif;"> toshokan</span>
+</nav>
+
+<Toshokan />
