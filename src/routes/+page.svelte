075e71663112ed885f62f7093e79add93b6af60c
Bruno Santos <bms.santos@outlook.pt>
2025-05-08 14:10:00 +0100

    Removed test file.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-    import Toshokan from "./toshokan.svelte";
     import HeroCarousel from "../components/hero-carousel.svelte";
 </script>
 
 <nav class="bg-white p-4 flex justify-center gap-2 dark:bg-gray-800 dark:text-white">
-    <span class="text-blue-300 text-3xl">図書館</span><span class="text-black text-3xl font-semibold" style="font-family: 'Times New Roman', serif;"> toshokan</span>
+    <span class="text-blue-300 text-3xl">図書館</span><span class="text-3xl font-semibold" style="font-family: 'Times New Roman', serif;"> toshokan</span>
 </nav>
 
 <HeroCarousel />
-<Toshokan />
