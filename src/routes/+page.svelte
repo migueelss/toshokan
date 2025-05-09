20c6d2c8f85562daf99f1ec970fadedac4ae4cdb
Bruno Santos <bms.santos@outlook.pt>
2025-05-09 14:58:42 +0100

    Adaptable! <3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import HeroCarousel from "../components/hero-carousel.svelte";
-    import AnimeList from "../components/animelist.svelte";
+    import MediaList from "../components/medialist.svelte";
 </script>
 
 <div class="fixed z-50 bg-gray-900 rounded-lg p-1 shadow-lg bottom-4 right-4 md:top-4 md:left-4 md:bottom-auto md:right-auto">
     <img src="/favicon.png" alt="Logo" class="w-12 h-12 object-cover rounded-lg" />
 </div>
 <HeroCarousel />
-<AnimeList type="ANIME" search="" perPage={50} genres={["Ecchi"]} sort={["SCORE_DESC"]}/>
+<MediaList type="ANIME" search="" perPage={50} genres={["Ecchi"]} sort={["SCORE_DESC"]}/>
