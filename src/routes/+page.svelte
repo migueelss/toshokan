fcdca9233b785b685179d6bcf597dadedfd51c10
Bruno Santos <bms.santos@outlook.pt>
2025-05-09 12:05:06 +0100

    Quickfix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import HeroCarousel from "../components/hero-carousel.svelte";
 </script>
 
-<div class="fixed z-50 bg-gray-800 rounded-lg p-1 shadow-lg bottom-4 right-4 md:top-4 md:left-4 md:bottom-auto md:right-auto">
+<div class="fixed z-50 bg-gray-900 rounded-lg p-1 shadow-lg bottom-4 right-4 md:top-4 md:left-4 md:bottom-auto md:right-auto">
     <img src="/favicon.png" alt="Logo" class="w-12 h-12 object-cover rounded-lg" />
   </div>
 <HeroCarousel />
