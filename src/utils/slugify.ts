13a27876b96ecacebc5a1cbe28d9b57b2314315d
Bruno Santos <bms.santos@outlook.pt>
2025-05-14 10:08:20 +0100

    brooming

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,8 +1,8 @@
 export default function slugify(str: string): string {
     return str
-      .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // remove acentos
-      .replace(/[^\w\s-]/g, "") // remove caracteres especiais
+      .normalize("NFD").replace(/[\u0300-\u036f]/g, "") 
+      .replace(/[^\w\s-]/g, "") 
       .trim()
-      .replace(/\s+/g, "-") // espaços por hífen
+      .replace(/\s+/g, "-")
       .toLowerCase();
 }
