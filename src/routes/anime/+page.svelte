e80fcf397d3f0f35d78b247aa252834bd69dd475
Bruno Santos <bms.santos@outlook.pt>
2025-05-14 16:24:22 +0100

    Testing content for pages

---
+++ src/routes/anime/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Medialist from "../../components/medialist.svelte";
+</script>
+
+
+<Medialist
+    mediaListTitle="TRENDING ANIME"
+    type="ANIME"
+    perPage={12}
+    sort={["TRENDING_DESC"]}
+    />
+
+<Medialist
+    mediaListTitle="BEST RATED ANIME"
+    type="ANIME"
+    perPage={24}
+    sort={["SCORE_DESC"]}
+    />
+
+<Medialist
+    mediaListTitle="BEST ACTION ANIME"
+    type="ANIME"
+    perPage={24}
+    sort={["POPULARITY_DESC"]}
+    genres={["Action"]}
+    />
---
+++ src/routes/manga/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Medialist from "../../components/medialist.svelte";
+</script>
+
+
+<Medialist
+    mediaListTitle="TRENDING MANGA"
+    type="MANGA"
+    perPage={12}
+    sort={["TRENDING_DESC"]}
+    />
+
+<Medialist
+    mediaListTitle="BEST RATED MANGA"
+    type="MANGA"
+    perPage={24}
+    sort={["SCORE_DESC"]}
+    />
+
+<Medialist
+    mediaListTitle="BEST ACTION MANGA"
+    type="MANGA"
+    perPage={24}
+    sort={["POPULARITY_DESC"]}
+    genres={["Action"]}
+    />
