ee7bd42e8dcbebfc2c62f81d0224a663cb306bc3
Bruno Santos <bms.santos@outlook.pt>
2025-05-14 13:10:40 +0100

    Testing on Adult Page

---
+++ src/routes/adult/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Medialist from "../../components/medialist.svelte";
+</script>
+
+<Medialist
+    mediaListTitle="ADULT ANIME"
+    type="ANIME"
+    perPage={12}
+    sort={["TRENDING_DESC"]}
+    isAdult={true} />
+
+<Medialist
+    mediaListTitle="ADULT MANGA"
+    type="MANGA"
+    perPage={24}
+    sort={["TRENDING_DESC"]}
+    isAdult={true}
+    countryOfOrigin={"JP"} />
+
+<Medialist
+    mediaListTitle="ADULT MANHWA"
+    type="MANGA"
+    perPage={24}
+    sort={["TRENDING_DESC"]}
+    isAdult={true}
+    countryOfOrigin={"KR"} />
